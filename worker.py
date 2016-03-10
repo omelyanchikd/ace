f8d1e54d842fcffcf2707f9a65fc95b9b5c13ee4
omelyanchikd <omelyanchikd@ukr.net>
2016-03-10 23:29:28 +0100

    Text change

--- worker.py
+++ worker.py
@@ -9,4 +9,4 @@ class Worker:
         if self.employer is not None:
             print("I am working for firm", self.employer, "with salary", self.salary)
         else:
-            print("I am jobless, homeless and unhappy, so looking for a job asap!")
+            print("I am jobless, homeless and unhappy, so will work for food!")
