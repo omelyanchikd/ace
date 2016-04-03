3be380bfef9bfa56eed44e66de763ee5d74d789c
Yurii Savka <urisavka@gmail.com>
2016-04-03 22:16:14 +0200

    fix regression

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,6 @@
 from stupid_firm import StupidFirm
 from basic_firm import BasicFirm
 from diana_firm import DianaFirm
+from super_firm import SuperFirm
 from ideal_world import IdealWorld
 from basic_world import BasicWorld
