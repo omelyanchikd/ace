80fd851f694471913419d74afd686bf949197bbd
omelyanchikd <omelyanchikd@ukr.net>
2016-04-05 22:21:43 +0200

    Created primitive statistics class for class World. First steps to save not only separate firm history, but aggregated world history.

--- stupid_firm.py
+++ stupid_firm.py
@@ -5,5 +5,6 @@ from firm_action import FirmAction
 class StupidFirm(Firm):
     def __init__(self, id):
         super().__init__(id)
-    def decide(self):
+
+    def decide(self, stats):
         return FirmAction(1, self.current_salary, self.stock, self.price, 0, 0, [])
