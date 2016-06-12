f4239f9af4c590454645ca8f6c9777f99048aca5
omelyanchikd <omelyanchikd@ukr.net>
2016-06-12 23:32:44 +0200

    Introducing QlearningFirm.

--- algorithms.py
+++ algorithms.py
@@ -4,6 +4,7 @@ from super_firm import SuperFirm
 from rule_firm import RuleFirm
 from moses_firm import MosesFirm
 from nonconscious_firm import NonconsciousFirm
+from qlearning_firm import QlearningFirm
 
 from ideal_world import IdealWorld
 from basic_world import BasicWorld
