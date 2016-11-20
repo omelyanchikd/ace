d7b78b946e8114eea79b57db0e3cded7272af41d
omelyanchikd <omelyanchikd@ukr.net>
2016-11-20 20:46:59 +0100

    Added neutral state

--- new_firm.py
+++ new_firm.py
@@ -19,6 +19,9 @@ class NewFirm(Firm):
         self.price_decrease_history = [1]
         self.plan_decrease_history = [1]
         self.salary_decrease_history = [1]
+        self.price_stable_history = [1]
+        self.plan_stable_history = [1]
+        self.salary_stable_history = [1]
         self.prev_price = 20
         self.prev_salary = 200
         self.prev_plan = 50 * self.efficiency_coefficient
@@ -40,6 +43,11 @@ class NewFirm(Firm):
                 self.price_decrease_history.append(1)
             else:
                 self.price_decrease_history.append(0)
+        if self.prev_price == self.price:
+            if self.profit >= 0:
+                self.price_stable_history.append(1)
+            else:
+                self.price_stable_history.append(0)
         if self.prev_plan < self.plan:
             if self.profit >= 0:
                 self.plan_increase_history.append(1)
@@ -60,43 +68,64 @@ class NewFirm(Firm):
                 self.salary_decrease_history.append(1)
             else:
                 self.salary_decrease_history.append(0)
+        if self.prev_plan == self.plan:
+            if self.profit >= 0:
+                self.plan_stable_history.append(1)
+            else:
+                self.plan_stable_history.append(0)
+        if self.prev_salary == self.salary:
+            if self.profit >= 0:
+                self.salary_stable_history.append(1)
+            else:
+                self.salary_stable_history.append(0)
+
         self.prev_price = self.price
         self.prev_plan = self.plan
         self.prev_salary = self.salary
         price_increase_probability = numpy.mean(self.price_increase_history)
         price_decrease_probability = numpy.mean(self.price_decrease_history)
+        price_stable_probability = numpy.mean(self.price_stable_history)
         salary_increase_probability = numpy.mean(self.salary_increase_history)
         salary_decrease_probability = numpy.mean(self.salary_decrease_history)
+        salary_stable_probability = numpy.mean(self.salary_stable_history)
         plan_increase_probability = numpy.mean(self.plan_increase_history)
         plan_decrease_probability = numpy.mean(self.plan_decrease_history)
+        plan_stable_probability = numpy.mean(self.plan_stable_history)
 
         total_salary = sum([worker.salary for worker in self.workers])
 
-        max_profit = 0
+        max_profit = plan_stable_probability * price_stable_probability * salary_stable_probability * self.profit
         max_price = self.price
         max_plan = self.plan
         max_salary = self.salary
 
-        for new_price in [0.95 * self.price, 1.05 * self.price]:
-            for new_plan in [self.plan - self.efficiency_coefficient, self.plan + self.efficiency_coefficient]:
-                for new_salary in [0.95 * self.salary, 1.05 * self.salary]:
+        for new_price in [0.95 * self.price, self.price, 1.05 * self.price]:
+            for new_plan in [self.plan - self.efficiency_coefficient, self.plan, self.plan + self.efficiency_coefficient]:
+                for new_salary in [0.95 * self.salary, self.salary, 1.05 * self.salary]:
                     new_profit = new_price * new_plan - total_salary - new_salary
                     if new_price > self.price:
                         new_profit *= price_increase_probability
-                    else:
+                    elif new_price < self.price:
                         new_profit *= price_decrease_probability
+                    else:
+                        new_profit *= price_stable_probability
                     if new_plan > self.plan:
                         new_profit *= plan_increase_probability
-                    else:
+                    elif new_plan < self.plan:
                         new_profit *= plan_decrease_probability
+                    else:
+                        new_profit *= plan_stable_probability
                     if new_salary > self.salary:
                         new_profit *= salary_increase_probability
-                    else:
+                    elif new_salary < self.salary:
                         new_profit *= salary_decrease_probability
+                    else:
+                        new_profit *= salary_stable_probability
                     if new_profit > max_profit:
                         max_price = new_price
                         max_salary = new_salary
                         max_plan = new_plan
+                        max_profit = new_profit
         self.price = max_price
         self.salary = max_salary
         self.plan = max_plan
