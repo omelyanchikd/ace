181a0a35fa3f8ca4b546d34c80f0a6f060c1d9aa
omelyanchikd <omelyanchikd@ukr.net>
2017-09-25 18:07:09 +0200

    Basic rule learning is now compatible with new JSON format

--- rule_firm.py
+++ rule_firm.py
@@ -14,10 +14,10 @@ def change(new_value, old_value):
     return (new_value - old_value)/old_value if old_value > 0 else 0
 
 class RuleFirm(DecisionMaker):
-    def __init__(self, id):
-        super().__init__(id)
-        self.salary = 200
-        self.plan = 50 * self.efficiency_coefficient
+    def __init__(self, id, firm):
+        super().__init__(id, firm)
+        self.salary = firm.salary
+        self.plan = firm.plan
 
         self.salary_change = 0
         self.price_change = 0
@@ -27,13 +27,13 @@ class RuleFirm(DecisionMaker):
         self.sales_change = 0
         self.profit_change = 0
 
-        self.prev_salary = self.salary
-        self.prev_price = self.price
-        self.prev_sold = self.sold
-        self.prev_plan = self.plan
-        self.prev_workers = len(self.workers)
-        self.prev_sales = self.sales
-        self.prev_profit = self.profit
+        self.prev_salary = firm.salary
+        self.prev_price = firm.price
+        self.prev_sold = firm.sold
+        self.prev_plan = firm.plan
+        self.prev_workers = len(firm.workers)
+        self.prev_sales = firm.sales
+        self.prev_profit = firm.profit
 
         self.rules_salary = [[0, 0.1, 0, 1, 0, 0.05],
                              [0.1, 0.3, 1, 3, 0, 0.05],
@@ -49,40 +49,43 @@ class RuleFirm(DecisionMaker):
         self.type = "RuleFirm"
 
 
-    def decide_salary(self, stats):
-        self.profit_change = change(self.profit, self.prev_profit)
-        self.sales_change = change(self.sales, self.prev_sales)
-        self.sold_change = change(self.sold, self.prev_sold)
+    def decide_salary(self, stats, firm):
+        self.profit_change = change(firm.profit, self.prev_profit)
+        self.sales_change = change(firm.sales, self.prev_sales)
+        self.sold_change = change(firm.sold, self.prev_sold)
         #self.workers_change = (len(self.workers) - self.prev_workers) / self.prev_workers if self.prev_workers > 0 else 0
-        self.workers_change = len(self.workers) - self.prev_workers
-        self.prev_salary = self.salary
-        self.prev_price = self.price
-        self.prev_plan = self.plan
-        self.prev_sold = self.sold
-        self.prev_workers = len(self.workers)
-        self.prev_sales = self.sales
-        self.prev_profit = self.profit
+        self.workers_change = len(firm.workers) - self.prev_workers
+
         #state = list(self.profit_change, self.sales_change, self.workers_change, self.sold_change)
         state = [self.sales_change, self.workers_change]
-        self.salary *= (1 + self.get_parameter(self.rules_salary, state))
-        self.salary = self.salary if self.salary > 0 else 0
+        firm.salary *= (1 + self.get_parameter(self.rules_salary, state))
+        firm.salary = firm.salary if firm.salary > 0 else 0
         #self.price *= (1 + self.price_change)
-        self.price = 1.05 * self.salary / self.efficiency_coefficient
-        self.plan = (1 + self.get_parameter(self.rules_plan, state)) * self.plan - self.stock
-        self.plan = self.plan if self.plan > 0 else 0
-        self.salary_change = change(self.salary, self.prev_salary)
-        self.price_change = change(self.price, self.prev_price)
-        self.plan_change = change(self.plan, self.prev_plan)
+        firm.price = 1.05 * firm.salary / firm.labor_productivity
+        firm.plan = (1 + self.get_parameter(self.rules_plan, state)) * firm.plan - firm.stock
+        firm.plan = firm.plan if firm.plan > 0 else 0
+        self.salary_change = change(firm.salary, self.prev_salary)
+        self.price_change = change(firm.price, self.prev_price)
+        self.plan_change = change(firm.plan, self.prev_plan)
 
-        self.offer_count = math.floor(self.plan / self.efficiency_coefficient) - len(self.workers)
+        self.prev_salary = firm.salary
+        self.prev_price = firm.price
+        self.prev_plan = firm.plan
+        self.prev_sold = firm.sold
+        self.prev_workers = len(firm.workers)
+        self.prev_sales = firm.sales
+        self.prev_profit = firm.profit
+
+        self.offer_count = math.floor(firm.plan / firm.labor_productivity) - len(firm.workers)
         while self.offer_count < 0:
-            self.fire_worker(random.choice(list(self.workers)))
+            firm.fire_worker(random.choice(list(firm.workers)))
             self.offer_count += 1
-        self.labor_capacity = len(self.workers) + self.offer_count
-        return FirmLaborMarketAction(self.offer_count, self.salary, [])
+        firm.labor_capacity = len(firm.workers) + self.offer_count
 
-    def decide_price(self, stats):
-        return FirmGoodMarketAction(self.stock, self.price, 0)
+        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
+
+    def decide_price(self, stats, firm):
+        return FirmGoodMarketAction(firm.stock, firm.price, 0)
 
     def get_parameter(self, rules, state):
         for rule in rules:
