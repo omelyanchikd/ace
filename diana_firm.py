9a401e835099fc831dcb9dac66e4d527c21dc2ba
omelyanchikd <omelyanchikd@ukr.net>
2017-09-25 14:29:28 +0200

    Intuitive learning now works for new JSON format

--- diana_firm.py
+++ diana_firm.py
@@ -9,48 +9,48 @@ import random
 
 
 class DianaFirm(DecisionMaker):
-    def __init__(self, id):
-        super().__init__(id)
-        self.plan = 50 * self.efficiency_coefficient
+    def __init__(self, id, firm):
+        super().__init__(id, firm)
+        self.plan = firm.plan
         self.prev_price = 1000
-        self.salary = 200
+        self.salary = firm.salary
         self.offer_count = 0
         self.type = 'DianaFirm'
 
-    def decide_salary(self, stats):
-        price_increase = self.prev_price < self.price
+    def decide_salary(self, stats, firm):
+        price_increase = self.prev_price < firm.price
         already_decreased = False
-        self.prev_price = self.price
-        if self.sold >= self.plan:
-            self.plan = self.sold + math.floor(self.efficiency_coefficient)
-            self.price *= 1.05
+        self.prev_price = firm.price
+        if firm.sold >= firm.plan:
+            firm.plan = firm.sold + math.floor(firm.labor_productivity)
+            firm.price *= 1.05
         else:
-            self.price *= 0.95
+            firm.price *= 0.95
             #if price_increase:
             #    self.price *= 0.95
             #    already_decreased = True
             #else:
             #    self.plan = self.sold if self.sold > 0 else self.efficiency_coefficient
         #self.plan = (self.plan - self.stock) // self.efficiency_coefficient * self.efficiency_coefficient
-        self.plan = self.plan // self.efficiency_coefficient * self.efficiency_coefficient
+        firm.plan = firm.plan // firm.labor_productivity * firm.labor_productivity
         #self.plan = self.plan if self.plan >= 0 else 0
-        self.offer_count = self.plan // self.efficiency_coefficient- len(self.workers)
+        self.offer_count = firm.plan // firm.labor_productivity - len(firm.workers)
         while self.offer_count < 0:
-            self.fire_worker(random.choice(list(self.workers)))
+            firm.fire_worker(random.choice(list(firm.workers)))
             self.offer_count += 1
-        total_salary = sum([worker.salary for worker in self.workers])
+        total_salary = sum([worker.salary for worker in firm.workers])
         while True:
             if self.offer_count > 0:
-                self.salary = 0.9 * (self.price * (len(self.workers) + self.offer_count) * self.efficiency_coefficient -
+                firm.salary = 0.9 * (firm.price * (len(firm.workers) + self.offer_count) * firm.labor_productivity  -
                               total_salary)/self.offer_count
-                if self.salary > 0:
+                if firm.salary > 0:
                     break
-                self.price *= 1.05
+                firm.price *= 1.05
             else:
                 break
         #print(str(self.salary) + " " + str(self.price * self.plan - total_salary - self.salary * self.offer_count))
-        self.labor_capacity = self.offer_count + len(self.workers)
-        return FirmLaborMarketAction(self.offer_count, self.salary, [])
+        firm.labor_capacity = self.offer_count + len(firm.workers)
+        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
 
-    def decide_price(self, stats):
-        return FirmGoodMarketAction(self.stock, self.price, 0)
+    def decide_price(self, stats, firm):
+        return FirmGoodMarketAction(firm.stock, firm.price, 0)
