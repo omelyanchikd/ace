0d3848323b48be3730a4a909dd66f88fd61e8f6f
omelyanchikd <omelyanchikd@ukr.net>
2017-09-27 15:30:41 +0200

    Constructor for CapitalFirm is now fully compatible with new JSON format. Now it is also possible to turn off some control parameters completely.

--- capital_firm.py
+++ capital_firm.py
@@ -1,3 +1,5 @@
+import math
+
 import algorithms
 from service import match
 
@@ -9,9 +11,20 @@ class CapitalFirm(Firm):
         super().__init__(id)
         self.id = id
         self.type = "CapitalFirm"
+        self.control_parameters = [parameter for parameter in model_config if model_config[parameter]]
+        if len(self.control_parameters) < 2:
+            raise AssertionError("Agent needs at least two defined control parameters to make decisions.")
+        self.derived_parameters = [parameter for parameter in model_config if model_config[parameter] is not None and not model_config[parameter]]
         for parameter in run_config:
-            if parameter not in model_config or model_config[parameter]:
+            if run_config[parameter] is None:
+                if parameter in self.derived_parameters:
+                    setattr(self, parameter, 0)
+                elif model_config[parameter] is not None:
+                    raise ValueError("Parameter " + parameter + " cannot be derived from others. Please define the parameter and restart the model.")
+            else:
                 setattr(self, parameter, run_config[parameter])
+        for parameter in self.derived_parameters:
+            setattr(self, parameter, self.derive(parameter, self.control_parameters))
         decision_maker = getattr(algorithms, match(learning_method))
         self.decision_maker = decision_maker(id, self)
 
@@ -23,4 +36,5 @@ class CapitalFirm(Firm):
             self.stock += len(self.workers) * self.labor_productivity
         for worker in self.workers:
             self.money -= worker.salary
+
 
