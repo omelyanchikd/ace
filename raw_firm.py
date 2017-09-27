29626774ad19c677c9453b0a145906a7bcad507c
omelyanchikd <omelyanchikd@ukr.net>
2017-09-27 14:24:48 +0200

    Derivation formulas for control parameters for RawFirm are now refactored in more readable format and available for all possible parameter combinations

--- raw_firm.py
+++ raw_firm.py
@@ -36,100 +36,131 @@ class RawFirm(Firm):
 
 
     def derive(self, parameter, control_parameters):
-        total_salary = sum([worker.salary for worker in self.workers])
-        if len(set(control_parameters) ^ set(['salary', 'price'])) == 0:
-            needed_workers = math.floor((total_salary * self.price - 1.05 * self.labor_productivity * len(self.workers)/
-                                         (1.05 * self.labor_productivity - self.price * self.salary)))
-            if needed_workers >= 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'plan':
-                    return math.floor((len(self.workers) + needed_workers) * self.labor_productivity)
-                if parameter == 'labor_capacity':
-                    return len(self.workers) + needed_workers
-                if parameter == 'salary_budget':
-                    return total_salary + self.salary * needed_workers
-            return 0
-        if len(set(control_parameters) ^ set(['salary', 'plan'])) == 0:
-            needed_workers = math.floor(self.plan/self.labor_productivity) - len(self.workers)
-            if needed_workers >= 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'price':
-                    return 1.05 * self.plan / (total_salary + self.salary * needed_workers)
-                if parameter == 'labor_capacity':
-                    return len(self.workers) + needed_workers
-                if parameter == 'salary_budget':
-                    return total_salary + self.salary * needed_workers
-            return 0
-        if len(set(control_parameters) ^ set(['price', 'plan'])) == 0:
+        if parameter == 'salary':
+            return self.derive_salary(control_parameters)
+        if parameter == 'price':
+            return self.derive_price(control_parameters)
+        if parameter == 'plan':
+            return self.derive_plan(control_parameters)
+        if parameter == 'labor_capacity':
+            return self.derive_labor_capacity(control_parameters)
+        if parameter == 'salary_budget':
+            return self.derive_salary_budget(control_parameters)
+        return 0
+
+
+    def derive_salary(self, control_parameters):
+        total_salary = [worker.salary for worker in self.workers]
+        if set(['price', 'plan']).issubset(control_parameters):
             needed_workers = math.floor(self.plan/self.labor_productivity) - len(self.workers)
             if needed_workers > 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'salary':
-                    return (1.05 * self.plan - total_salary * self.price)/(self.price * needed_workers)
-                if parameter == 'labor_capacity':
-                    return len(self.workers) + needed_workers
-                if parameter == 'salary_budget':
-                    return 1.05 * self.plan / self.price
+                return (1.05 * self.plan - total_salary * self.price)/(self.price * needed_workers)
             return 0
-        if len(set(control_parameters) ^ set(['salary', 'labor_capacity'])) == 0:
-            needed_workers = self.labor_capacity - len(self.workers)
-            if needed_workers >= 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'price':
-                    return 1.05 * self.labor_productivity * self.labor_capacity / (total_salary + self.salary * needed_workers)
-                if parameter == 'plan':
-                    return self.labor_productivity * self.labor_capacity
-                if parameter == 'salary_budget':
-                    return total_salary + self.salary * needed_workers
-            return 0
-        if len(set(control_parameters) ^ set(['price', 'labor_capacity'])) == 0:
+        if set(['price', 'labor_capacity']).issubset(control_parameters):
             needed_workers = self.labor_capacity - len(self.workers)
             if needed_workers > 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'salary':
-                    return (1.05 * self.labor_productivity * self.labor_capacity - total_salary * self.price)/(self.price * needed_workers)
-                if parameter == 'plan':
-                    return self.labor_productivity * self.labor_capacity
-                if parameter == 'salary_budget':
-                    return 1.05 * self.labor_productivity * self.labor_capacity / self.price
+                return (1.05 * self.labor_capacity * self.labor_productivity -
+                        total_salary * self.price) / (self.price * needed_workers)
             return 0
-        if len(set(control_parameters) ^ set(['salary', 'salary_budget'])) == 0:
-            needed_workers = math.floor((self.salary_budget - total_salary)/self.salary)
-            if needed_workers >= 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'price':
-                    return 1.05 * self.labor_productivity * (len(self.workers) + needed_workers) / self.salary_budget
-                if parameter == 'plan':
-                    return (len(self.workers) + needed_workers) * self.labor_productivity
-                if parameter == 'labor_capacity':
-                    return len(self.workers) + needed_workers
+        if set(['plan', 'salary_budget']).issubset(control_parameters):
+            needed_workers = math.floor(self.plan/self.labor_productivity) - len(self.workers)
+        elif set(['labor_capacity', 'salary_budget']).issubset(control_parameters):
+            needed_workers = self.labor_capacity - len(self.workers)
+        elif set(['price', 'salary_budget']).issubset(control_parameters):
+            needed_workers = (self.price * self.salary_budget - 1.05 * self.labor_productivity * len(self.workers)) / \
+                             (1.05 * self.labor_productivity)
+        else:
             return 0
-        if len(set(control_parameters) ^ set(['price', 'salary_budget'])) == 0:
-            needed_workers = (self.price * self.salary_budget - 1.05 * self.labor_productivity * len(self.workers)) / (1.05 * self.labor_productivity)
-            if needed_workers > 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'salary':
-                    return (self.salary_budget - total_salary) / needed_workers
-                if parameter == 'plan':
-                    return self.price * self.salary_budget/1.05
-                if parameter == 'labor_capacity':
-                    return self.price * self.salary_budget/(1.05 * self.labor_productivity)
-            return 0
-        if len(set(control_parameters) ^ set(['plan', 'labor_capacity'])) == 0:
-            raise ValueError('Plan and labor capacity are not enough to derive all other control parameters')
-            return 0
-        if len(set(control_parameters) ^ set(['plan', 'salary_budget'])) == 0:
+        if needed_workers > 0:
+            return (self.salary_budget - total_salary)/needed_workers
+        return 0
+
+
+    def derive_price(self, control_parameters):
+        total_salary = [worker.salary for worker in self.workers]
+        if set(['salary', 'plan']).issubset(control_parameters):
             needed_workers = math.floor(self.plan/self.labor_productivity) - len(self.workers)
             if needed_workers > 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'salary':
-                    return (self.salary_budget - total_salary) / needed_workers
-                if parameter == 'price':
-                    return self.salary_budget/(1.05 * self.plan)
-                if parameter == 'labor_capacity':
-                    return len(self.workers) + needed_workers
+                return 1.05 * self.plan / (total_salary + self.salary * needed_workers)
             return 0
-        if len(set(control_parameters) ^ set(['labor_capacity', 'salary_budget'])) == 0:
+        if set(['salary', 'labor_capacity']).issubset(control_parameters):
             needed_workers = self.labor_capacity - len(self.workers)
             if needed_workers > 0 or len(self.workers) + needed_workers > 0:
-                if parameter == 'salary':
-                    return (self.salary_budget - total_salary) / needed_workers
-                if parameter == 'price':
-                    return self.salary_budget/(1.05 * self.labor_capacity * self.labor_productivity)
-                if parameter == 'plan':
-                    return self.labor_capacity * self.labor_productivity
+                return 1.05 * self.labor_capacity * self.labor_productivity / (total_salary + self.salary * needed_workers)
+            return 0
+        if set(['plan', 'salary_budget']).issubset(control_parameters):
+            needed_workers = math.floor(self.plan/self.labor_productivity) - len(self.workers)
+            if needed_workers > 0:
+                return 1.05 * self.plan / self.salary_budget
+            return 0
+        if set(['labor_capacity', 'salary_budget']).issubset(control_parameters):
+            needed_workers = self.labor_capacity - len(self.workers)
+            if needed_workers > 0:
+                return 1.05 * self.labor_capacity * self.labor_productivity / self.salary_budget
+            return 0
+        if set(['salary', 'salary_budget']).issubset(control_parameters):
+            needed_workers = math.floor((self.salary_budget - total_salary) / self.salary)
+            if needed_workers > 0:
+                return 1.05 * self.labor_productivity * (len(self.workers) + needed_workers) / self.salary_budget
             return 0
         return 0
+
+
+    def derive_plan(self, control_parameters):
+        total_salary = [worker.salary for worker in self.workers]
+        if set(['labor_capacity']).issubset(control_parameters):
+            return math.floor(self.labor_capacity * self.labor_productivity)
+        if set(['salary', 'price']).issubset(control_parameters):
+            needed_workers = math.floor((total_salary * self.price - 1.05 * self.labor_productivity * len(self.workers)/
+                                         (1.05 * self.labor_productivity - self.price * self.salary)))
+        elif set(['salary', 'salary_budget']).issubset(control_parameters):
+            needed_workers = math.floor((self.salary_budget - total_salary) / self.salary)
+        elif set(['price', 'salary_budget']).issubset(control_parameters):
+            needed_workers = (self.price * self.salary_budget - 1.05 * self.labor_productivity * len(self.workers)) / \
+                             (1.05 * self.labor_productivity)
+        else:
+            return 0
+        if needed_workers > 0 or len(self.workers) + needed_workers > 0:
+            return math.floor((len(self.workers) + needed_workers) * self.labor_productivity)
+        return 0
+
+
+    def derive_labor_capacity(self, control_parameters):
+        total_salary = [worker.salary for worker in self.workers]
+        if set(['plan']).issubset(control_parameters):
+            return math.floor(self.plan / self.labor_productivity)
+        if set(['salary', 'price']).issubset(control_parameters):
+            needed_workers = math.floor((total_salary * self.price - 1.05 * self.labor_productivity * len(self.workers)/
+                                         (1.05 * self.labor_productivity - self.price * self.salary)))
+        elif set(['salary', 'salary_budget']).issubset(control_parameters):
+            needed_workers = math.floor((self.salary_budget - total_salary) / self.salary)
+        elif set(['price', 'salary_budget']).issubset(control_parameters):
+            needed_workers = (self.price * self.salary_budget - 1.05 * self.labor_productivity * len(self.workers)) / \
+                             (1.05 * self.labor_productivity)
+        else:
+            return 0
+        if needed_workers > 0 or len(self.workers) + needed_workers > 0:
+            return len(self.workers) + needed_workers
+        return 0
+
+
+    def derive_salary_budget(self, control_parameters):
+        total_salary = [worker.salary for worker in self.workers]
+        if set(['price', 'plan']).issubset(control_parameters):
+            return 1.05 * self.plan / self.price
+        if set(['price', 'labor_capacity']).issubset(control_parameters):
+            return 1.05 * self.labor_capacity * self.labor_productivity / self.price
+        if set(['salary', 'plan']).issubset(control_parameters):
+            needed_workers = math.floor(self.plan / self.labor_productivity) - len(self.workers)
+        elif set(['salary', 'labor_capacity']).issubset(control_parameters):
+            needed_workers = self.labor_capacity - len(self.workers)
+        elif set(['salary', 'price']).issubset(control_parameters):
+            needed_workers = math.floor((total_salary * self.price - 1.05 * self.labor_productivity * len(self.workers) /
+                 (1.05 * self.labor_productivity - self.price * self.salary)))
+        if needed_workers > 0 or len(self.workers) + needed_workers > 0:
+            return total_salary + self.salary * needed_workers
+        return 0
+
+
+
 
