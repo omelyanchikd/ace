0858c780db725874ecc637bdf995df1fa215a64c
omelyanchikd <omelyanchikd@ukr.net>
2017-10-04 21:15:15 +0200

    Method of making decisions "ClassificationTreeFirm" is now able to adapt to all possible control parameters of Firm

--- tree_firm.py
+++ tree_firm.py
@@ -18,76 +18,60 @@ class TreeFirm(DecisionMaker):
         super().__init__(id, firm)
         self.type = 'TreeFirm'
         self.salary = firm.salary
-        recorded_history = pandas.read_csv("change_history.csv", sep = ";", decimal = ",")
-        self.world_history = recorded_history.as_matrix(columns = ['price', 'salary', 'sold', 'workers', 'world_price', 'world_salary',
-                                                                   'rest_money', 'world_sold'])
-        self.world_history = self.world_history.tolist()
-        self.profit_history = list(recorded_history['has_profit'])
+        #recorded_history = pandas.read_csv("change_history.csv", sep = ";", decimal = ",")
+        #self.world_history = recorded_history.as_matrix(columns = ['price', 'salary', 'sold', 'workers', 'world_price', 'world_salary',
+        #                                                           'rest_money', 'world_sold'])
+        #self.world_history = self.world_history.tolist()
+        #self.profit_history = list(recorded_history['has_profit'])
         self.decision_tree = tree.DecisionTreeClassifier()
-        self.decision_tree.fit(self.world_history, self.profit_history)
-        #self.neural_network = Perceptron(fit_intercept=False)
-        #self.neural_network.fit(self.world_history, self.profit_history)
-        #self.neural_network = MLPClassifier(hidden_layer_sizes=(30, ))
-        #self.neural_network.fit(self.world_history, self.profit_history)
-        self.plan = firm.plan
-        self.offer_count = 0
-        self.prev_state = [19.9, 199.9, 500, 50, 20, 200, 0, 5000]
-        self.current_state = [] * 8
-        self.change = [] * 8
+        self.world_history = []
+        self.profit_history = []
+        #self.decision_tree.fit(self.world_history, self.profit_history)
 
-    def update_history(self, stats, firm):
-        self.current_state = [firm.price, firm.salary, firm.sold, len(firm.workers), stats.price, stats.salary,
-                              stats.money - stats.sales, stats.sold]
-        self.change = [(self.current_state[i] - self.prev_state[i])/self.prev_state[i] if self.prev_state[i] != 0 else 0 for i in range(0, len(self.prev_state)) ]
-        self.prev_state = [firm.price, firm.salary, firm.sold, len(firm.workers), stats.price, stats.salary,
-                           stats.money - stats.sales, stats.sold]
+#        self.prev_state = [19.9, 199.9, 500, 50, 20, 200, 0, 5000]
+#        self.current_state = [] * 8
+#        self.change = [] * 8
 
-        #self.world_history.append([self.price, self.salary, self.sold, len(self.workers), stats.price, stats.salary,
-        #                           stats.money - stats.sales, stats.employed, stats.sold])
+    def update_history(self, stats, firm, type):
+#        self.current_state = [firm.price, firm.salary, firm.sold, len(firm.workers), stats.price, stats.salary,
+#                              stats.money - stats.sales, stats.sold]
+#        self.change = [(self.current_state[i] - self.prev_state[i])/self.prev_state[i] if self.prev_state[i] != 0 else 0 for i in range(0, len(self.prev_state)) ]
+#        self.prev_state = [firm.price, firm.salary, firm.sold, len(firm.workers), stats.price, stats.salary,
+#                           stats.money - stats.sales, stats.sold]
 
-        self.world_history.append(self.change)
+        self.world_history.append(firm.control_parameters + [getattr(stats, type + '_price'), getattr(stats, type + '_salary')])
         self.profit_history.append(1 if firm.profit > 0 else 0)
 
-    def generate_parameters(self, stats):
-        new_parameters = [0, 0, 0]
+    def generate_parameters(self, stats, firm, type):
+        new_parameters = [getattr(firm, parameter) for parameter in firm.control_parameters]
         trial_parameters = []
         for parameter in new_parameters:
-            change = random.gauss(0, 0.333)
-           # parameter *= (1 + change)
-           # trial_parameters.append(parameter)
-            trial_parameters.append(change)
-        #trial_parameters.append(math.ceil(new_parameters[2] / self.efficiency_coefficient))
-        trial_parameters.append(trial_parameters[2])
-        #for parameter in [stats.price, stats.salary, stats.money - stats.sales, stats.sold]:
-        for parameter in self.change[4:]:
-        #    trial_parameters.append(0)
+            change = random.gauss(0, 1)
+            parameter *= (1 + change)
             trial_parameters.append(parameter)
-
+        #trial_parameters.append(math.ceil(new_parameters[2] / self.efficiency_coefficient))
+        for parameter in [getattr(stats, type + '_price'), getattr(stats, type + '_salary')]:
+            trial_parameters.append(parameter)
         return new_parameters, trial_parameters
 
 
     def decide_salary(self, stats, firm):
-        self.update_history(stats, firm)
-        #current_data = [self.price, self.salary, self.sold, len(self.workers), stats.price, stats.salary, stats.money - stats.sales]
-        self.decision_tree.fit(self.world_history, self.profit_history)
-        #self.neural_network.fit(self.world_history, self.profit_history)
+        type = firm.type[:len(firm.type) - 4].lower()
+        self.update_history(stats, firm, type)
+        current_data = firm.control_parameters + [getattr(stats, type + '_price'), getattr(stats, type + '_salary')]
+        if len(set(self.profit_history)) == 2:
+            self.decision_tree.fit(self.world_history, self.profit_history)
         for i in range(0, 100):
-            new_parameters, trial_parameters = self.generate_parameters(stats)
-            #has_profit = self.neural_network.predict(trial_parameters)
-            has_profit = self.decision_tree.predict(trial_parameters)
+            new_parameters, trial_parameters = self.generate_parameters(stats, firm, type)
+            if len(set(self.profit_history)) == 2:
+                has_profit = self.decision_tree.predict(trial_parameters)
+            else:
+                has_profit = random.randint(0,1)
             if has_profit == 1:
-                new_parameters = (trial_parameters[0], trial_parameters[1], trial_parameters[2])
+                new_parameters = trial_parameters[:len(firm.control_parameters)]
                 break
-        #self.price, self.salary, self.plan = [for value in new_parameters]
-        firm.plan = (new_parameters[2] + 1) * firm.plan if (new_parameters[2] + 1) * firm.plan > 0 else firm.labor_productivity
-        firm.price = (new_parameters[0] + 1) * firm.price if (new_parameters[0] + 1) * firm.price > 0 else 20
-        firm.salary = (new_parameters[1] + 1) * firm.salary if (new_parameters[1] + 1) * firm.salary > 0 else 200
-        self.offer_count = math.floor(firm.plan / firm.labor_productivity) - len(firm.workers)
-        while self.offer_count < 0:
-            firm.fire_worker(random.choice(list(firm.workers)))
-            self.offer_count += 1
-        firm.labor_capacity = len(firm.workers) + self.offer_count
-        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
+        for i, parameter in enumerate(firm.control_parameters):
+            setattr(firm, parameter, new_parameters[i])
 
     def decide_price(self, stats, firm):
         return FirmGoodMarketAction(firm.stock, firm.price, 0)
