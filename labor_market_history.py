8389343c45c327b1647008e05bf7f6fdbf1cde5f
omelyanchikd <omelyanchikd@ukr.net>
2017-10-07 23:09:58 +0200

    Created class LaborMarketHistory to track all changes in firm-worker interactions

---
+++ labor_market_history.py
@@ -0,0 +1,27 @@
+import csv
+
+class LaborMarketHistory:
+    def __init__(self, output = "labor_market_output.csv"):
+        self.output = output
+        for variable in ['step', 'worker_id', 'employer_id', 'action', 'salary']:
+            setattr(self, variable, [])
+        try:
+            open(self.output, "r")
+        except:
+            output_file = open(self.output, "w", newline='')
+            writer = csv.DictWriter(output_file, dialect = 'excel',
+                                    fieldnames=['step', 'worker_id', 'employer_id', 'action', 'salary'])
+            writer.writeheader()
+            output_file.close()
+
+
+
+    def add_record(self, record):
+        for variable, value in record.items():
+            getattr(self, variable).append(value)
+        with open(self.output, "a", newline='') as output_file:
+            writer = csv.DictWriter(output_file, dialect='excel', fieldnames=['step', 'worker_id', 'employer_id', 'action', 'salary'])
+            writer.writerow(record)
+
+
+
