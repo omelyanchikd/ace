b1473d4473e22092262971f979e9b899e7b35411
omelyanchikd <omelyanchikd@ukr.net>
2017-10-10 18:06:40 +0200

    Converting ace to python package

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import algorithms
+
+from world import World
+from firm import Firm
+
+from ace import run
+
+from firm_goodmarket_action import FirmGoodMarketAction
+from firm_labormarket_action import FirmLaborMarketAction
+
+from firm_goodmarket_result import FirmGoodMarketResult
+from firm_labormarket_result import FirmLaborMarketResult
+
+from firm_history import FirmHistory
+from world_history import WorldHistory
+
+from good_market_history import GoodMarketHistory
+from labor_market_history import LaborMarketHistory
+
+from firm_action import FirmAction
+from firm_result import FirmResult
+
+
+
