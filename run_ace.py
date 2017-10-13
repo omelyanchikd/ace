3c7fec5aab31032cd2ddda58fc27cbea928b62ff
omelyanchikd <omelyanchikd@ukr.net>
2017-10-13 22:44:20 +0200

    Splitting run_ace into two functions: one able to read from json-string, other able to read from json-file

--- run_ace.py
+++ run_ace.py
@@ -9,7 +9,22 @@ import ace.algorithms
 #with open('run_config.json', 'r') as f:
 #    run_config = json.load(f)
 
-def run_ace(model_config_json, run_model_config_json):
+def run_ace_from_file(model_config_json, run_model_config_json):
+    with open(model_config_json, 'r') as f:
+        model_config = json.load(f)
+
+    with open(run_model_config_json, 'r'):
+        run_config = json.load(open(run_model_config_json, 'r'))
+
+    model_config = model_config[0]['fields']
+    run_config = run_config[0]['fields']
+
+    world = ace.algorithms.BasicWorld(model_config, run_config)
+    history = world.go()
+    return history
+
+
+def run_ace_from_json(model_config_json, run_model_config_json):
     model_config = json.loads(model_config_json)
     run_config = json.loads(run_model_config_json)
 
@@ -19,7 +34,6 @@ def run_ace(model_config_json, run_model_config_json):
     world = ace.algorithms.BasicWorld(model_config, run_config)
     history = world.go()
     return history
-
 
 #random.seed(config['global']['seed'])
 #algorithm_class = config['global']['world_algorithm']
