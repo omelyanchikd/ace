cf3bfe5a2883377d1b6bc1dc3c9ad8fb414eb2ff
omelyanchikd <omelyanchikd@ukr.net>
2017-10-17 19:41:59 +0200

    IntuitiveFirm algorithm is now able to handle presence of raw and capital control parameters

--- intuitive_firm.py
+++ intuitive_firm.py
@@ -12,12 +12,8 @@ import numpy
 class IntuitiveFirm(DecisionMaker):
     def __init__(self, id, firm):
         super().__init__(id, firm)
-        self.salary = firm.salary
-        self.prev_workers = 50
-        self.offer_count = 0
         self.period = 3
         self.history = [1000, 1000, 1000]
-        self.plan = firm.plan
         self.smoothing_coefficient = 0.5
         self.type = 'Extrapolation'
 
@@ -32,19 +28,17 @@ class IntuitiveFirm(DecisionMaker):
             firm.plan = self.smoothing_coefficient * firm.sold + (1 - self.smoothing_coefficient) * numpy.mean(self.history)
         self.history.append(firm.sold)
         self.history.pop(0)
-        firm.plan = firm.plan // firm.labor_productivity * firm.labor_productivity
-        firm.plan = firm.plan if firm.plan > 0 else firm.labor_productivity
-        if len(firm.workers) < self.prev_workers + self.offer_count:
+        if len(firm.workers) < firm.labor_capacity:
             firm.salary *= 1.01
         else:
             firm.salary *= 0.99
-        self.offer_count = firm.plan // firm.labor_productivity - len(firm.workers)
-        while self.offer_count < 0:
-            firm.fire_worker(random.choice(list(firm.workers)))
-            self.offer_count += 1
-        self.prev_workers = len(firm.workers)
-        total_salary = sum([worker.salary for worker in firm.workers])
-        firm.price = 1.05 * (total_salary + firm.salary)/firm.plan
-        firm.price = firm.price if firm.price > 0 else 0
-        firm.labor_capacity = len(firm.workers) + self.offer_count
-        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
+        control_parameters = ['plan', 'salary']
+        if hasattr(firm, 'raw'):
+            firm.raw_budget = stats.raw_price * math.floor(firm.plan / firm.raw_productivity)
+            control_parameters.append('raw_budget')
+        if hasattr(firm, 'capital'):
+            firm.capital_budget = stats.capital_price * math.floor(firm.plan / firm.capital_productivity)
+            control_parameters.append('capital_budget')
+        for parameter in firm.control_parameters:
+            firm.__setattr__(parameter, firm.derive(parameter, control_parameters))
+
