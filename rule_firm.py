29c13132943fee17da1c8e490ea32293c4033550
omelyanchikd <omelyanchikd@ukr.net>
2017-10-18 15:25:42 +0200

    RuleFirm algorithm is now able to handle presence of raw and capital control parameters

--- rule_firm.py
+++ rule_firm.py
@@ -11,78 +11,42 @@ def isinrange(value, left, right):
     return left <= value < right
 
 def change(new_value, old_value):
-    return (new_value - old_value)/old_value if old_value > 0 else 0
+    return (new_value - old_value)/old_value if old_value != 0 else 0
+
+def generate_sample_rule(rule_length):
+    return [random.uniform(-1, 1) for i in range(rule_length * 2 + 2)]
+
 
 class RuleFirm(DecisionMaker):
     def __init__(self, id, firm):
         super().__init__(id, firm)
-        self.salary = firm.salary
-        self.plan = firm.plan
 
-        self.salary_change = 0
-        self.price_change = 0
-        self.sold_change = 0
-        self.workers_change = 0
-        self.plan_change = 0
-        self.sales_change = 0
-        self.profit_change = 0
+        self.external = ['sales', 'sold', 'profit', 'workers']
+        if hasattr(firm, 'raw'):
+            self.external.append('raw')
+        if hasattr(firm, 'capital'):
+            self.external.append('capital')
 
-        self.prev_salary = firm.salary
-        self.prev_price = firm.price
-        self.prev_sold = firm.sold
-        self.prev_plan = firm.plan
-        self.prev_workers = len(firm.workers)
-        self.prev_sales = firm.sales
-        self.prev_profit = firm.profit
+        rules_n = 10
 
-        self.rules_salary = [[0, 0.1, 0, 1, 0, 0.05],
-                             [0.1, 0.3, 1, 3, 0, 0.05],
-                             [-0.1, 0, 0, 1, -0.05, 0],
-                             [-0.3, -0.1, 0, 1, -0.1, -0.05]]
-        self.rules_plan = [[0, 0.1, 0, 1, 0, 0.1],
-                           [0.1, 0.3, 1, 3, 0, 0.05],
-                           [-0.1, 0, 0, 1, -0.1, 0],
-                           [-0.3, -0.1, 0, 1, -0.3, -0.1]]
+        for parameter in firm.parameters:
+            setattr(self, 'rules_' + parameter, [generate_sample_rule(len(self.external)) for i in range(rules_n)])
 
-        self.offer_count = 0
+        self.change = [0] * len(self.external)
+        self.state = {parameter: getattr(firm, parameter) for parameter in self.external}
 
         self.type = "RuleFirm"
 
-
     def decide_salary(self, stats, firm):
-        self.profit_change = change(firm.profit, self.prev_profit)
-        self.sales_change = change(firm.sales, self.prev_sales)
-        self.sold_change = change(firm.sold, self.prev_sold)
-        #self.workers_change = (len(self.workers) - self.prev_workers) / self.prev_workers if self.prev_workers > 0 else 0
-        self.workers_change = len(firm.workers) - self.prev_workers
+        self.update_state(firm)
 
-        #state = list(self.profit_change, self.sales_change, self.workers_change, self.sold_change)
-        state = [self.sales_change, self.workers_change]
-        firm.salary *= (1 + self.get_parameter(self.rules_salary, state))
-        firm.salary = firm.salary if firm.salary > 0 else 0
-        #self.price *= (1 + self.price_change)
-        firm.price = 1.05 * firm.salary / firm.labor_productivity
-        firm.plan = (1 + self.get_parameter(self.rules_plan, state)) * firm.plan - firm.stock
-        firm.plan = firm.plan if firm.plan > 0 else 0
-        self.salary_change = change(firm.salary, self.prev_salary)
-        self.price_change = change(firm.price, self.prev_price)
-        self.plan_change = change(firm.plan, self.prev_plan)
+        for parameter in firm.control_parameters:
+            value = getattr(firm, parameter)
+            if parameter in ['plan', 'labor_capacity', 'raw_need', 'capital_need']:
+                firm.__setattr__(parameter, math.floor(value * (1 + self.get_parameter(getattr(self, 'rules_' + parameter), self.change))))
+            else:
+                firm.__setattr__(parameter, value * (1 + self.get_parameter(getattr(self, 'rules_' + parameter), self.change)))
 
-        self.prev_salary = firm.salary
-        self.prev_price = firm.price
-        self.prev_plan = firm.plan
-        self.prev_sold = firm.sold
-        self.prev_workers = len(firm.workers)
-        self.prev_sales = firm.sales
-        self.prev_profit = firm.profit
-
-        self.offer_count = math.floor(firm.plan / firm.labor_productivity) - len(firm.workers)
-        while self.offer_count < 0:
-            firm.fire_worker(random.choice(list(firm.workers)))
-            self.offer_count += 1
-        firm.labor_capacity = len(firm.workers) + self.offer_count
-
-        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
 
     def decide_price(self, stats, firm):
         return FirmGoodMarketAction(firm.stock, firm.price, 0)
@@ -96,3 +60,9 @@ class RuleFirm(DecisionMaker):
                 return random.uniform(rule[2 * len(state)], rule[2 * len(state) + 1])
         select = random.randint(0, len(rules) - 1)
         return random.uniform(rules[select][2 * len(state)], rules[select][2 * len(state) + 1])
+
+    def update_state(self, firm):
+        self.change = [change(getattr(firm, parameter), self.state[parameter]) for parameter in self.external]
+        self.state = {parameter:getattr(firm, parameter) for parameter in self.external}
+
+
