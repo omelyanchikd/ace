3e8c23803b7ed9fdc131bc8359ab81286605e2e6
omelyanchikd <omelyanchikd@ukr.net>
2017-10-20 16:51:07 +0200

    MosesFirm is now available to use with all possible combinations of control parameters

--- moses_firm.py
+++ moses_firm.py
@@ -7,86 +7,111 @@ from .firm_goodmarket_action import FirmGoodMarketAction
 import math
 import random
 
+def expected_profit_margin(firm, expected):
+    raw_budget = firm.raw_budget if hasattr(firm, 'raw_budget') else 0
+    capital_budget = firm.capital_budget if hasattr(firm, 'capital_budget') else 0
+    capital_amortization = firm.capital_amortization if hasattr(firm, 'capital_amortization') else 0
+
+    return (expected - firm.salary_budget - raw_budget - capital_amortization * capital_budget)/expected
 
 def change(new_value, old_value):
-    return (new_value - old_value) / old_value if old_value > 0 else 0
+    if isinstance(new_value, set):
+        return (len(new_value) - len(old_value))/len(old_value) if len(old_value) != 0 else 0
+    return (new_value - old_value)/old_value if old_value != 0 else 0
 
 
-def check_margin(salary, workers, expected):
-    return 1 - salary * workers / expected > 0.05 if expected > 0 else 0
+def check_margin(firm, control_parameters, derived_parameters, expected):
+    for parameter in derived_parameters:
+        setattr(firm, parameter, firm.derive(parameter, control_parameters))
+    return expected_profit_margin(firm, expected) > 0.05 if expected > 0 else 0
 
 
 class MosesFirm(DecisionMaker):
     def __init__(self, id, firm):
         super().__init__(id, firm)
-        self.salary = firm.salary
-        self.plan = firm.plan
 
-        self.salary_change = 0
-        self.price_change = 0
         self.sold_change = 0
-        self.workers_change = 0
-        self.plan_change = 0
         self.sales_change = 0
-        self.profit_change = 0
+        self.raw_price_change = 0
+        self.stats_raw_price_change = 0
+        self.capital_price_change = 0
+        self.stats_capital_price_change = 0
 
-        self.prev_salary = firm.salary
-        self.prev_price = firm.price
         self.prev_sold = firm.sold
-        self.prev_plan = firm.plan
-        self.prev_workers = len(firm.workers)
         self.prev_sales = firm.sales
-        self.prev_profit = firm.profit
+        self.prev_raw_price = 0
+        self.prev_capital_price = 0
+        self.stats_raw_price_change = 0
+        self.stats_capital_price_change = 0
 
         self.exp_sales = 0.1
         self.exp_sold = 0.1
+        self.exp_raw_price = 0.1
+        self.exp_capital_price = 0.1
 
         self.expected = 0
         self.type = "MosesFirm"
 
     def decide_salary(self, stats, firm):
+        control_parameters = ['plan', 'price', 'salary']
+        derived_parameters = ['labor_capacity', 'salary_budget']
+
         self.sales_change = change(firm.sales, self.prev_sales)
         self.sold_change = change(firm.sold, self.prev_sold)
 
         self.prev_sold = firm.sold
         self.prev_sales = firm.sales
 
-        expected_sales_growth = 0.05 # Get correct value for expected_sales_growth from stats
-        expected_sold_growth = 0.05  # Get correct value for expected_sold_growth from stats
-
-        self.exp_sales = 0.5 * self.sales_change + 0.5 * expected_sales_growth
-        self.exp_sold = 0.5 * self.sold_change + 0.5 * expected_sold_growth
+        self.exp_sales = 0.5 * self.sales_change + 0.5 * stats.expected_sales_growth
+        self.exp_sold = 0.5 * self.sold_change + 0.5 * stats.expected_sold_growth
 
         self.expected = (1 + self.exp_sales) * firm.sales
-        self.plan = (1 + self.exp_sold) * firm.sold
-
         self.expected = self.expected if self.expected >= 0 else 0
 
-        firm.plan = (firm.plan - firm.stock) // firm.labor_productivity * firm.labor_productivity
-        firm.plan = firm.plan if firm.plan >= 0 else 0
-
+        firm.plan = (1 + self.exp_sold) * firm.sold
         firm.price = self.expected / firm.plan if firm.plan > 0 and self.expected > 0 else firm.price
-        firm.salary = 0.95 * firm.price * firm.labor_productivity
 
-        while not (
-        check_margin(firm.salary, firm.plan // firm.labor_productivity, self.expected)) and self.expected != 0:
+        if hasattr(firm, 'raw'):
+            self.raw_price_change = change(firm.raw_budget/firm.raw, self.prev_raw_price)
+            self.stats_raw_price_change = change(stats.raw_price, self.stats_raw_price_change)
+            self.exp_raw_price = 0.5 * self.raw_price_change + 0.5 * self.stats_raw_price_change
+            self.prev_raw_price = firm.raw_budget/firm.raw
+            self.prev_stats_raw_price = stats.raw_price
+
+            firm.raw_budget = firm.plan / firm.raw_productivity * (1 + self.exp_raw_price) * stats.raw_price
+            control_parameters.append('raw_budget')
+            derived_parameters.append('raw_need')
+
+        if hasattr(firm, 'capital'):
+            self.capital_price_change = change(firm.capital_budget / firm.capital, self.prev_capital_price)
+            self.stats_capital_price_change = change(stats.capital_price, self.stats_capital_price_change)
+            self.exp_capital_price = 0.5 * self.capital_price_change + 0.5 * self.stats_capital_price_change
+            self.prev_capital_price = firm.capital_budget/firm.capital
+            self.prev_stats_capital_price = stats.capital_price
+
+            firm.capital_budget = (firm.plan / firm.capital_productivity - firm.capital) * stats.capital_price * (1 + self.capital_raw_price)
+            control_parameters.append('capital_budget')
+            derived_parameters.append('capital_need')
+
+        firm.salary = firm.derive_salary(control_parameters)
+
+        tries = 0
+
+        while not (check_margin(firm, control_parameters, derived_parameters, self.expected)) and self.expected != 0 and tries < 100:
             if firm.profit >= 0:
                 firm.salary *= 0.95
             else:
-                firm.plan = firm.plan // firm.labor_productivity - 1
+                firm.plan = math.floor(firm.plan / firm.labor_productivity) - 1
                 firm.price = self.expected / firm.plan if firm.plan > 0 and self.expected > 0 else firm.price
-                firm.salary = 0.95 * firm.price * firm.labor_productivity
-        firm.plan = firm.plan if firm.plan >= 0 else 0
-        self.offer_count = math.floor(firm.plan / firm.labor_productivity) - len(firm.workers)
-        while self.offer_count < 0:
-            firm.fire_worker(random.choice(list(firm.workers)))
-            self.offer_count += 1
-        if firm.salary == 0:
-            while (len(firm.workers) > 0):
-                worker = list(firm.workers)[0]
-                firm.fire_worker(worker)
-        firm.labor_capacity = len(firm.workers) + self.offer_count
-        return FirmLaborMarketAction(self.offer_count, firm.salary, [])
+                if hasattr(firm, 'raw'):
+                    firm.raw_budget = firm.plan / firm.raw_productivity * self.exp_raw_price
+
+                if hasattr(firm, 'capital'):
+                    firm.capital_budget = (firm.plan / firm.capital_productivity - firm.capital) * self.capital_raw_price
+
+                firm.salary = firm.derive_salary(control_parameters)
+            tries += 1
+
 
     def decide_price(self, stats, firm):
         return FirmGoodMarketAction(firm.stock, firm.price, 0)
